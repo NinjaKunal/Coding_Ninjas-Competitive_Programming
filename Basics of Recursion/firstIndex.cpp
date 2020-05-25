b03191a5dfc822c278955d509319737245855556
NinjaKunal <56207577+NinjaKunal@users.noreply.github.com>
2020-05-25 18:01:28 +0530

    Create firstIndex.cpp

---
+++ Basics of Recursion/firstIndex.cpp
@@ -0,0 +1,33 @@
+//FIRST INDEX OF NUMBER
+
+/*
+Given an array of length N and an integer x, you need to find and return the first index of integer x present in the array. Return -1 if it is not present in the array.
+First index means, the index of first occurrence of x in the input array.
+Do this recursively. Indexing in the array starts from 0.
+Input Format :
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Line 3 : Integer x
+Output Format :
+first index or -1
+Constraints :
+1 <= N <= 10^3
+Sample Input :
+4
+9 8 10 8
+8
+Sample Output :
+1
+*/
+
+int firstIndex(int input[], int size, int x,int curr=0) {
+     if(size==curr){
+        return -1;
+    }
+
+    if(input[curr] == x){
+        return curr;
+    }
+
+    return firstIndex(input,size,x,curr+1);
+}

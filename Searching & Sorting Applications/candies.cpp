4a9c4feb12f3708954e18ae077207357d43d7a90
NinjaKunal <56207577+NinjaKunal@users.noreply.github.com>
2020-05-27 19:00:27 +0530

    Create candies.cpp

---
+++ Searching & Sorting Applications/candies.cpp
@@ -0,0 +1,61 @@
+//DISTRIBUTE CANDIES
+
+/*
+Shaky has N (1<=N<=50000) candy boxes each of them contains a non-zero number of candies (between 1 and 1000000000). Shaky want to distibute these candies among his K (1<=K<=1000000000) IIIT-Delhi students. He want to distibute them in a way such that:
+1. All students get equal number of candies.
+2. All the candies which a student get must be from a single box only.
+As he want to make all of them happy so he want to give as many candies as possible. Help Shaky in finding out what is the maximum number of candies which a student can get.
+Input
+First line contains 1<=T<=20 the number of test cases. Then T test cases follow. First line of each test case contains N and K. Next line contains N integers, ith of which is the number of candies in ith box.
+Output
+For each test case print the required answer in a seperate line.
+Sample Input:
+2
+3 2 
+3 1 4
+4 1
+3 2 3 9
+Sample Output:
+3
+9
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+	int t;
+	ll k,x,n,i,s,e,m,p;
+	vector<ll> v;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n>>k;
+		ll *a=new ll[n];
+		for(i=0;i<n;++i)
+			cin>>a[i];
+		sort(a,a+n);
+		s=0,e=a[n-1],x=0;
+		while(s<=e)
+		{
+			p=0;
+			m=(s+e)/2;
+			for(i=0;i<n;++i)
+			{
+				p+=a[i]/m;
+			}
+			if(p>=k)
+				{
+					x=m;
+					s=m+1;
+				}
+			else
+				e=m-1;		
+		}
+		cout<<x<<endl;
+		delete a;
+	}
+	return 0;
+}
